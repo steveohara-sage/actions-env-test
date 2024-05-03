d6115170e89c16a0cecb4689adab5546e29e1d0b
Steve O'Hara <steve.ohara@sage.com>
2024-05-03 10:16:59 +0100

    initial version

---
+++ src/test/java/com/test/MainTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
+}
